411f1b6a6a4ba1f83b6748c169346ad88e225cf5
YIHAN WANG <yw7451@nyu.edu>
2023-09-16 21:15:35 -0400

    colorful shapes

--- sketch.js
+++ sketch.js
@@ -1,5 +1,22 @@
-function setup() {
-}
-
-function draw() {
-}
+  function setup() {
+    createCanvas(800, 1040);
+  }
+  
+  function draw() {
+    background("#F1F2F0");
+    fill(0);
+    noStroke();
+    quad(27,195,63,238,220,109,184,61);
+    fill("#3760BA");
+    quad(114,196,226,436,469,269,270,66);
+    fill("#864736");
+    quad(571,254,572,335,663,335,661,251);
+    fill("#EABA2C");
+    quad(612,335,612,569,755,571,757,333);
+    fill("#20282A");
+    quad(224,629,224,987,582,987,585,629);
+    noFill();
+    stroke("#864736");
+    strokeWeight(20);
+    ellipse(520,385,100,90);
+  }
